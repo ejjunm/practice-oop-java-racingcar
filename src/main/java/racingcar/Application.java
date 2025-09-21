9c94c7350cec9bd797d3ec765a80d5b0a33c1c6a
ejjun <jyj020516@gmail.com>
2025-09-21 21:52:08 +0900

    [0주차] 6기 객체지향 코드 연습(전예준)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+        String[] carList = View.name();
+		int num = View.count();
+		
+		ArrayList<Car> cars = new ArrayList<>();
+		for(String name : carList) {
+			cars.add(new Car(name));
+		}
+		
+		System.out.println("실행 결과\n");
+		
+		for(int i = 0; i < num; i++) {
+			for (Car car : cars) {
+                car.move();
+            }
+			View.roundPrint(cars);
+		}
+		
+		View.getWinner(cars);
+	}
 }
